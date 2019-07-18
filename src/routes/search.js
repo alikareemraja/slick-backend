5bfd142fb23d48b79528c97d67f55756102cf6b7
ahabes <abdullah096@hotmail.com>
2019-07-18 15:41:39 +0200

    add search statistics route

--- src/routes/search.js
+++ src/routes/search.js
@@ -3,15 +3,16 @@ const express = require("express");
 const router = express.Router();
 const middlewares = require("../middlewares");
 const ItemController = require("../controllers/item");
-//const StatisticsController = require("../controllers/statistics");
+const StatisticsController = require("../controllers/statistics");
 
 router.get("/get", middlewares.checkAuthentication, ItemController.read_search); // search for Item
-//router.get("/getstat", middlewares.checkAuthentication, StatisticsController.list); // GET search statistics
-//router.post( // keep track of performed search operations
-//  "/addstat",
-//  middlewares.checkAuthentication,
-//  StatisticsController.create
-//);
+router.get("/stat", middlewares.checkAuthentication, StatisticsController.list); // GET search statistics
+router.post(
+  // keep track of performed search operations
+  "/addstat",
+  middlewares.checkAuthentication,
+  StatisticsController.create
+);
 
 router.post("/", middlewares.checkAuthentication, ItemController.create); // Add Item to wardrobe
 
